37afb34602c4761f8b5372c4e0cd6f709eaa7ea2
chris <chris@ryoko.localdomain>
2009-06-22 20:19:52 -0400

    knocked out basics of the gradient descent method, not tested

--- src/maximise.c
+++ src/maximise.c
@@ -1,5 +1,7 @@
 #include "maximise.h"
 
+#define FAILVALUE -100
+
 /** 
  * @file 
  * @author Chris Coleman-Smith cec24@phy.duke.edu
@@ -16,10 +18,12 @@
  * @param ranges -> a matrix (nthetas * 2) where each row is the max and min allowable range to search for hyperparams in, use this to seed the 
  * initial search etc.
  */
-void gradDesc(int max_tries, int nsteps, int gamma, gsl_matrix* ranges,  gsl_matrix *xmodel, gsl_vector *trainingvector, gsl_vector *thetas, int nmodel_points, int nthetas, int nparams){
+void gradDesc(gsl_rng *rand, int max_tries, int nsteps, int gamma, gsl_matrix* ranges,  gsl_matrix *xmodel, gsl_vector *trainingvector, gsl_vector *thetas, int nmodel_points, int nthetas, int nparams){
 	int i,j;
 	int tries = 0;
 	double best_value = 0.0;
+	double temp_val = 0.0;
+
 	// vectors for the grad desc
 	gsl_vector *gradient_vec = gsl_vector_alloc(nthetas);
 	gsl_vector *xNew = gsl_vector_alloc(nthetas);
@@ -36,7 +40,7 @@ void gradDesc(int max_tries, int nsteps, int gamma, gsl_matrix* ranges,  gsl_mat
 	int lu_signum = 0;
 	
 	while(tries < max_tries){
-		set_random_initial_value(xOld, ranges, nthetas);
+		set_random_initial_value(rand, xOld, ranges, nthetas);
 		
 		for(i = 0; i < nsteps; i++){
 			
@@ -48,25 +52,127 @@ void gradDesc(int max_tries, int nsteps, int gamma, gsl_matrix* ranges,  gsl_mat
 			
 			// now get the determinant of the inverse
 			// actually don't need that here, need it when we're seeing to keep the estimate
-			//		gsl_matrix_memcpy(temp_matrix, cinverse);
+			//gsl_matrix_memcpy(temp_matrix, cinverse);
 			//gsl_linalg_LU_decomp(temp_matrix, c_LU_permutation, &lu_signum);
 			//cinverse_det = gsl_linalg_LU_det(temp_matrix, lu_signum);
 
 			for(j = 0; j < nthetas; j++){ /* calc the gradient vector*/
-				the_gradient =  getGradient(cinverse, xmodel, trainingvector, thetas, i, nmodel_points, nthetas, nparams);
-				gsl_vector_set(gradient_vec, i, the_gradient);
+				the_gradient =  getGradient(cinverse, xmodel, trainingvector, xOld, j, nmodel_points, nthetas, nparams);
+				gsl_vector_set(gradient_vec, j, the_gradient);
 			}
 			
+			// xNew = xOld + gamma*the_gradient(xold)
+			// there's probably a neat function for this but i can't be bothered
+			for(j = 0; j < nthetas; j++){
+				temp_val = gsl_vector_get(xOld, j) + gamma*gsl_vector_get(the_gradient, j);
+				gsl_vector_set(xNew, j, temp_val);
+			}
+			
+			if( range_check(xNew, ranges, nthetas) == 1){
+				gsl_vector_memcpy(xOld, xNew); // set the next xOld value
+			} else {
+				for(j = 0; j < nthetas; j++){
+					gsl_vector_set(xNew, j, FAILVALUE);
+				}
+				break(); // get out of this loop, it didn't work
+			}
+		}
+		tries++;
+		if ( vector_components_equal(xNew, FAILVALUE) != 1){
+			// the thing didn't get set to the failval
+			// now we want to calc the logLikelyhood and see how it is
+			gsl_matrix_memcpy(temp_matrix, cinverse);
+			gsl_linalg_LU_decomp(temp_matrix, c_LU_permutation, &lu_signum);
+			cinverse_det = gsl_linalg_LU_det(temp_matrix, lu_signum);
+			// temp_val is now the likelyhood for this answer
+			temp_val = getLogLikelyhood(cinverse, cinverse_det, xmodel, trainingvector, xNew, nmodel_points, nthetas, nparams);
+			
+			if(temp_val > best_value){
+				//debug
+				printf("best: %g\n", best_value);
+				gsl_vector_memcpy(best_vector, xNew);
+			}			
+		}
+	}
+	
+	// now we copy the best value into thetas
+	gsl_vector_memcpy(thetas, best_vector);
+	
+	// clean up extra junk
+	gsl_vector_free(best_vector);
+	gsl_vector_free(xOld);
+	gsl_vector_free(xNew);
+	gsl_vector_free(gradient_vec);
+	
+	gsl_matrix_free(covariance_matrix);
+	gsl_matrix_free(cinverse);
+	gsl_matrix_free(temp_matrix);
+	gsl_permutation_free(c_LU_permutation);
+	
+}
 
+//! see if a given vector has it's components in the correct range
+/**
+ * @return returns 1 if true
+ */
+int range_check(gsl_vector* x, gsl_matrix* ranges, int nthetas){
+	int i, j;
+	double range_min;
+	double range_max;
+	int truecount = 0;
+	for(i = 0; i < nthetas; i++){
+		range_min = gsl_matrix_get(ranges, i, 0);
+		range_max = gsl_matrix_get(ranges, i, 1);
+		if (gsl_vector_get(x, i) <= range_max && gsl_vector_get < range_min){
+			truecount++;
+		}
+	}
+	if (truecount == nthetas){
+		// all values of x are in range
+		return(1);
+	} else {
+		return(0);
+	}
+}
 
-
-
-
-
-
-
-
-
+//! see if a given vector has all of its components equal to the given value
+/**
+ * @return 1 if the test passes, zero otherwise 
+ */
+int vector_components_equal(gsl_vector *x, double test_value, int nthetas){
+	int i;
+	int truecount = 0;
+	for(i = 0; i < nthetas; i++){
+		// double comparision, but should be ok here
+		if(gsl_vector_get(x, i) == test_value){
+			truecount++;
+		}
+	}	
+	if(truecount == nthetas){
+		return(1);
+	} else {
+		return(0);
+	}
+}
+ 
+	
+//! set a random starting point in nthetas dimensions, sampled from the ranges
+/**
+ * @param rand a pre setup gsl_rng
+ * @return x is set to the correct values
+ */
+void set_random_initial_value(gsl_rng* rand, gsl_vector* x, gsl_matrix* ranges,int  nthetas){
+	int i;
+	double range_min; 
+	double range_max;
+	
+	for(i = 0; i < nthetas; i++){
+		range_min = gsl_matrix_get(ranges, i, 0);
+		range_max = gsl_matrix_geT(ranges, i, 1);
+		gsl_vector_set(x, gsl_rng_uniform(rand)*(range_max-range_min)+range_min);
+	}
+}
+	
 
 
 
