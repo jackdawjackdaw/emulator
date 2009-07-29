4661c3646d54bed2a0e1fb3b5b7c88b1bcf9483c
jackdawjackdaw <cec24@phy.duke.edu>
2009-07-29 15:17:24 -0400

    changed filename of 1d-test-data.R and added code for a simple 2d test data set

---
+++ r-fns/emulator-test-data.R
@@ -0,0 +1,48 @@
+library(lhs)
+
+                                        
+# generate a sample data set that can be used to test the emluator
+# this generates a noisy flatline which snaps into a messy sin/cos wave
+# at 35.7
+test1d <- function(outname, numbersamples){
+  answer <- rep(NA, numbersamples)
+  x <- seq(0, 60, length=numbersamples)
+  for(i in 1:numbersamples){
+    answer[i] <- t1d(x[i]) + rnorm(1,0, 0.1)
+  }
+  write("# 1d test inputfile for emualtor", file=outname, append=FALSE)
+write.table(answer, file=outname, append=TRUE, sep=" ", col.names=FALSE, row.names=FALSE)
+}
+
+ t1d <- function(x){
+   temp <- theta(x, 35.75)
+   print(temp)
+   (sin(pi*x/5)+(1/5)*cos(4*pi*x/5))*temp
+ }
+
+ theta <- function(x, y){
+   if( x >= y){
+     1.0
+   } else {
+     0.0
+   }
+ }
+
+# another test function, for 2d data
+# y(x) = x1*exp(-x1^2 - x2^2)
+# this is ranges on [-2,2], [-2, 2]
+test2dexpo <- function(outname, numbersamples){
+  samplepoints <- maximinLHS(numbersamples, 2)
+  samplepoints <- (samplepoints * 4 - 2)
+  temp <- rep(NA, numbersamples)
+  for(i in 1:numbersamples){
+    temp[i] <- y2d(samplepoints[i,])
+  }
+  answer <- data.frame(x=samplepoints[,1], y=samplepoints[,2], ans=temp)
+  write("# 2d test inputfile for emulator", file=outname, append=FALSE)
+  write.table(answer, file=outname,append=TRUE, sep=" ", col.names=FALSE, row.names=FALSE)
+}
+
+y2d <- function(x){
+  x[1]*exp(-x[1]^2- x[2]^2)
+}
