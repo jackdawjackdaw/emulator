fe7077e4ef314371959b86270de06ca4a1056ee7
jackdawjackaw <cec24@phy.duke.edu>
2009-11-25 23:24:54 -0500

    updated testRbind to let it set different options and try out diff cov fns

--- r-fns/testRbind.R
+++ r-fns/testRbind.R
@@ -1,6 +1,17 @@
 dyn.load("../src/libRbind.so")
 source("emulator-test-data.R")
 library("lhs")
+library("scatterplot3d")
+
+## some test functions and the interface to rbind (at the end of the file)
+## the main ones of interest are callcode and setEmulatorOptions
+##
+## setEmulatorOptions -> lets you change the covariance function and lets you change the alpha in the cov fn
+## you have to call this sometime before you run callcode! if you don't want to change things you should use
+## setDefaultOptions (as in the examples)
+##
+## callcode -> takes a data frame for the model (and some other stuff) and produces another data frame with the emulated
+## mean and variance
 
 # this is a simple example of how to use the callcode function
 # we load some data from our general test file and then call the
@@ -9,10 +20,46 @@ testIsing <- function(){
   model <- readIsingData()
   nmodelpts <- dim(model)[1]
   nemupts <- 100
+  setDefaultOps()
   ans<-callcode(model, nmodelpts, nemupts=nemupts)
   plotResults(model, ans)  
   #ans
 }
+
+
+## plots an array of grophs, toprow using lhs sampled input
+## bottom row using uniformly sampled input
+##
+## interstingly (uninterestingly) the uniformly sampled input
+## really really sucks
+makeSuperPlot <- function(){
+  par(mfrow=c(2,5))
+  #postscript("psuperPlot.ps")
+  for(i in 1:5)
+    testNModelPts(i+3,1)
+  for(i in 1:5)
+    testNModelPts(i+3,0)
+  #dev.off()
+}
+
+
+compareCovFns <- function(){
+  par(mfrow=c(1,2))
+  m <- 8
+  # use the gaussian cov fn
+  testNModelPts(m, lhs=1)
+  # now set the matern cov fn
+  setEmulatorOptions(0,1.9)
+  model <- demoModel(m, lhs=1)
+  nmodelpts <- m
+  nemupts <-  200
+  ansMatern <- callcode(model, nmodelpts, nemupts=nemupts, rangemin=0.0, rangemax=1.0)
+  sequence <- seq(0.0,1.0, length=nemupts)
+  actual <- data.frame(x=sequence, y=yM(sequence))
+  plotResultsTest(model, ansMatern, actual)
+}
+                       
+
 
 
 ## for making some demo plots
@@ -34,28 +81,21 @@ demoModel <- function(m, lhs=0){
   model
 }
 
-makeSuperPlot <- function(){
-  par(mfrow=c(2,5))
-  postscript("superPlot.ps")
-  for(i in 1:5)
-    testNModelPts(i+3,1)
-  for(i in 1:5)
-    testNModelPts(i+3,0)
-  dev.off()
-}
-
-
 testNModelPts <- function(m, lhs=0){
   model <- demoModel(m, lhs=lhs)
   nmodelpts <- m
   nemupts <- 200
   print(model)
+  setDefaultOps()
   ans <- callcode(model, nmodelpts, nemupts=nemupts, rangemin=0.0, rangemax=1.0)
   sequence <- seq(0.0,1.0, length=nemupts)
   actual <- data.frame(x=sequence, y=yM(sequence))
   plotResultsTest(model, ans, actual)
 
 }
+
+
+
 
 # super super slow(if you use matern)
 # same as plot results but now we have an analytic actual model to plot
@@ -73,7 +113,6 @@ plotResultsTest <- function(model, results, actual){
   grid()
 }
 
-
       
 plotResults <- function(model, results){
   # this works in 1d now
@@ -89,11 +128,42 @@ plotResults <- function(model, results){
 }
 
 
+yGauss <- function(x,y,x0,y0, a,b,c){
+  f <- a*(x-x0)^2 + 2*b*(x-x0)*(y-y0) + c*(y-y0)^2
+  return(exp(-f))
+}
+
+
+## these gauss functions don't work yet
+## a nice plot can be made : scatterplot3d(demoGauss(20), highlight.3d=TRUE, pch=20)
+## but you can't rotate the little guy :(
+demoGauss <- function(m){
+  # this is a 2d func to we'll use a LHS in 2d
+  params <- maximinLHS(m,2)
+  result <- rep(0,m)
+  for(i in 1:m){
+    result[i] <- yGauss(params[i,1], params[i,2],0.5,0.5,100,0.0,100) +
+      yGauss(params[i,1], params[i,2],0.1,0.1,75,0,75)
+  }
+  model <- data.frame(xmodel=params, training=result)
+  model
+}
+
+runGauss <-function(m){
+  nmodelpts <- m
+  model <- demoGauss(nmodelpts)
+  nemupts <- 200
+  nthetas <- 5
+  ans <- callcode(model, nmodelpts, nemupts=200, nparams=2, nthetas=nthetas)
+}
+
+
 
 
 test1dSin <- function(){
   nmodelpts <- 40
   model <- test1dRCall(nmodelpts)
+  setDefaultOps()
   ans<-callcode(model, nmodelpts, rangemin=0.0, rangemax=60, nemupts=100)
   plotResults(model, ans)
 }
@@ -112,6 +182,22 @@ testLikely <- function(){
   l
 }
 
+setDefaultOps <- function(){
+  setEmulatorOptions(1,1.9)
+}
+
+
+## i've taken away the call in the C function, so you need to do this
+## before you CALL CODE or DEATH
+# the function we're calling has no return, it just switches
+# some options around so this should be pretty simple
+setEmulatorOptions <- function(useGauss, alpha){
+  foo<-.C("setEmulatorOptions",
+     as.integer(useGauss),
+     as.double(alpha))
+
+}
+
 
 # i don't quite understand how to push the results together into a data
 # frame i should check on this.
@@ -119,7 +205,9 @@ testLikely <- function(){
 # not sure how to grab the results?
 callcode <- function(model, nmodelpts, nparams=1, nthetas=4, nemupts=50, rangemin=0.0, rangemax=4.0){
 
-res<-  .C("callEmulator",
+  if(nparams==1){
+  
+    res<-  .C("callEmulator",
      as.double(model$xmodel),
      as.integer(nparams),
      as.double(model$training),
@@ -131,6 +219,33 @@ res<-  .C("callEmulator",
      finalvar = double(nemupts),
      as.double(rangemin),
      as.double(rangemax))
+  }else if(nparams==2){
+    newmodel <- rep(NA, 2*nmodelpts)
+    print(model)
+    # interleave the xmodel array
+    for(i in 1:nmodelpts)
+      newmodel[2*i-1] <- model$xmodel.1[i]
+    for(i in 1:nmodelpts)
+      newmodel[2*i] <- model$xmodel.2[i]
+    print(newmodel)
+    
+    res<-  .C("callEmulator",
+     as.double(newmodel),
+     as.integer(nparams),
+     as.double(model$training),
+     as.integer(nmodelpts),
+     as.integer(nthetas),
+     finalx = double(nemupts*nemupts),
+     as.integer(nemupts),
+     finaly = double(nemupts),
+     finalvar = double(nemupts),
+     as.double(rangemin),
+     as.double(rangemax))
+    
+
+  } else {
+    print("sorry, won't work with nparams > 2")
+  }
 
   results <- data.frame(emulatedx=res$finalx[1:nemupts], emulatedy=res$finaly, emulatedvar=res$finalvar)
   results
