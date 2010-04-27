fee7547b836ca8e9fd7a54714662377580530046
Chris C-S <cec24@phy.duke.edu>
2010-04-26 22:40:37 -0400

    updated make samples to produce a 6d set too, this will hopefully be a bit more well behaved in the estimator

--- src/libSA/make-samples.R
+++ src/libSA/make-samples.R
@@ -35,3 +35,28 @@ designFrame <- data.frame(x=params, y=designVal)
 write.table(designFrame,"15d-oak-ohagan-data-sample.txt", row.names=FALSE, col.names=FALSE)
 
 
+## now make the reduced data set
+ndimred <- 6
+a1red <- c(a1[1], a1[2], a1[6], a1[7], a1[11], a1[12])
+a2red <- c(a2[1], a2[2], a2[6], a2[7], a2[11], a2[12])
+a3red <- c(a3[1], a3[2], a3[6], a3[7], a3[11], a3[12])
+
+mred <- matrix(0, nrow=6, ncol=6)
+removeVec <- c(-3,-4,-5,-8,-9,-10,-13,-14,-15)
+for(i in 1:ndimred)
+  mred[i,] <- m[i,removeVec]
+
+yvalred <- function(x){
+  a1red %*% x + a2red %*% (sin(x)) + a3red %*% (cos(x)) + x %*% mred %*% x
+}
+
+paramsred  <- maximinLHS(sampleSize, ndimred)*5 - 2.5
+yvalsreduced <- rep(0, sampleSize)
+for(i in 1:sampleSize)
+  yvalsreduced[i] <- yvalred(paramsred[i,])
+
+
+designFrameRed <- data.frame(x=paramsred, y=yvalsreduced)
+
+write.table(designFrameRed,"6d-oak-ohagan-data-sample.txt", row.names=FALSE, col.names=FALSE)
+
