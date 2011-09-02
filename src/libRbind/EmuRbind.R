0b94094f64d62651c38ed8adcf18a0da776dadf2
jackdawjackdaw <cec24@phy.duke.edu>
2011-09-02 15:02:18 -0400

    EmuRbind now supports cov.fn and reg.ord which set the covariance and regression options, added a LOT of commentary

--- src/libRbind/EmuRbind.R
+++ src/libRbind/EmuRbind.R
@@ -1,12 +1,47 @@
 library("lhs")
 
+## ccs, cec24@phy.duke.edu (2011-2008)
+##
+## functions for calling the emulator through R
+##
+## common parameters
+## 
+## @param model: a list/data frame with entries: xmodel & training
+## @param model$xmodel: the design positions where the model was evaluated (nparams cols, nmodelpoints rows)
+## @param model$training: the values of the model at each of the positions in xmodel
+## @param nparams: number of design dimensions
+## @param nthetas: number of hyperparams for the emulator (needs to agree with the cov-fn) 
+##
+## @param reg.order sets the order of the linear regression model applied,
+## where 0: a0, 1: a0 + a1*x etc
+##
+## @param cov.fn {1,2,3} selects the covariance function from { power_exp, matern_32, matern_52 }
+## the cov.fns require nthetas as such:
+## power_exp -> nparams + 2 
+## matern_32 -> 3
+## matern_52 -> 3
+## 
+## the thetas must always be stored as: {amplitude, nugget, length_scales...}
+## amplitude -> overall scale applied to the cov fn
+## nugget -> (iid noise term) applied to the diagonal
+## length_scales -> for power_exp there are nparams of these one for each direction,
+## for the matern class there is a single length scale
+
  
-## estimates the thetas for a model 
-# if fixedNugget is not set to NULL the supplied value is used to fix the nugget
-# for the estimation process. This can be used to force some uncertainty in the training points
-# also it can just fuck things up majorly
-callEstimate <- function(model, nmodelpts,nparams=1, nthetas=3, fixedNugget=NULL){
+## estimates the thetas for a model, do this first to create your emulator
+## 
+## @param fixedNugget: force the nugget to be within 5% of the given value, don't if NULL
+## @return "optimal" estimated thetas for the supplied model, cov.fn, reg.order combo
+##
+## the created emulator is really the set: {model, thetas (which come from here) and reg.oder and cov.fn)
+## so to use the emulator to make predictions at locations you'll need to send the same {model, thetas, reg.order and cov.fn)
+## 
+## 
+callEstimate <- function(model, nmodelpts,nparams=1, nthetas=3, fixedNugget=NULL, cov.fn=1, reg.order=1){
   #browser()
+
+  checkCovFn(nthetas, nparams, cov.fn)
+  
   if(is.null(fixedNugget)){
     res <- .C("callEstimate",
               as.double((model$xmodel)),
@@ -15,7 +50,8 @@ callEstimate <- function(model, nmodelpts,nparams=1, nthetas=3, fixedNugget=NULL
               as.integer(nmodelpts),
               as.integer(nthetas),
               thetas = double(nthetas),
-              as.integer(0), as.double(0))
+              as.integer(0), as.double(0),
+              as.integer(cov.fn), as.integer(reg.order))
   } else {
     res <- .C("callEstimate",
               as.double((model$xmodel)),
@@ -24,7 +60,8 @@ callEstimate <- function(model, nmodelpts,nparams=1, nthetas=3, fixedNugget=NULL
               as.integer(nmodelpts),
               as.integer(nthetas),
               thetas = double(nthetas),
-              as.integer(1), as.double(fixedNugget))
+              as.integer(1), as.double(fixedNugget),
+              as.integer(cov.fn), as.integer(reg.order))
   }
   res$thetas
 }
@@ -32,16 +69,26 @@ callEstimate <- function(model, nmodelpts,nparams=1, nthetas=3, fixedNugget=NULL
 ## 
 ## the idea is to take a model in a few (ideally-inde) y-dimensions (factored into principle cpts or whatever in another fn)
 ## and emulate/estimate each dimension as if it was a single scalar field over the parameters.
-# nydims -> the number of separate training dimensions
-# training -> matrix of the y-values for each separate dimension
-# model-> the parameter space over which we've evaluated all of the training vectors
-# nmodelpts -> the length of each training vector, the number of points at which the model was evaluated
-multidim <-  function(model, nmodelpts, training, nydims, fixedNugget=NULL){
+# @param nydims -> the number of separate training dimensions
+# @param training -> matrix of the y-values for each separate dimension
+#
+# @param fixedNugget see above
+#
+# @return matrix of optimal thetas one row per training dimension
+#
+multidim <-  function(model, nmodelpts, training, nydims, fixedNugget=NULL, cov.fn=1, reg.order=1){
   nparams <- ncol(as.matrix(model))
-  nthetas <- nparams+2 # this is the correct number for the gaussian cov fn
+  
+  if(cov.fn == 1){
+    nthetas <- nparams+2 # this is the correct number for the gaussian cov fn
+  } else {
+    nthetas <- 3
+  }
+  
   bigthetas <- array(0, dim=c(nydims, nthetas)) # store all the thetas
 
-  
+  checkCovFn(nthetas, nparams, cov.fn)
+
   for(i in 1:nydims){ # estimate the thetas for each sub-model
     # we have to craft a custom frame for each call
     if(nydims > 1){
@@ -61,7 +108,17 @@ multidim <-  function(model, nmodelpts, training, nydims, fixedNugget=NULL){
 ## return the emulated mean and var at point
 ## N calls of this are substantially slower than using callEmulateAtList,
 ## the AtList function is much more efficient.
-callEmulateAtPoint <- function(model, thetas, point, nmodelpts, nparams=1, nthetas=3){
+#
+# @param point the locn to emulate the model at
+# 
+# @return a list res containing fields {des, mean, var}
+# @return res$des location at which we obtain the emulated mean and variance
+# @return res$mean emulated mean at res$des
+# @return res$var emulated variance at res$des
+callEmulateAtPoint <- function(model, thetas, point, nmodelpts, nparams=1, nthetas=3,
+                               cov.fn=1, reg.order=1){
+
+  checkCovFn(nthetas, nparams, cov.fn)
   #browser()
   res <- .C("callEmulateAtPt",
             as.double((model$xmodel)),
@@ -72,14 +129,24 @@ callEmulateAtPoint <- function(model, thetas, point, nmodelpts, nparams=1, nthet
             as.double(thetas),
             as.integer(nthetas),
             finaly = double(1),
-            finalvar = double(1)
+            finalvar = double(1),
+            as.integer(cov.fn),
+            as.integer(reg.order)
             )
   results <- list(des=point, mean=res$finaly, var=res$finalvar)
 }
 
 
 ## the preferred way to compue the emulated mean and variance at a set of points given by pointList
-callEmulateAtList <- function(model, thetas, pointList,nemupts, nmodelpoints, nparams=1, nthetas=3){
+##
+## @param pointList an R matrix (nrow=nemupts, ncol=nparams) of points at which to emulate the model
+## @param nemupts how many points to compute
+## @return a list res(des, mean, var)
+## @return res$des<-pointList
+## @return res$mean emulated mean at each location in pointList
+## @return res$var emulated var at each location in pointList
+callEmulateAtList <- function(model, thetas, pointList,nemupts, nmodelpoints, nparams=1, nthetas=3,
+                              cov.fn=1, reg.order=1){
   
   res <- .C("callEmulateAtList",
             as.double((model$xmodel)),
@@ -91,13 +158,26 @@ callEmulateAtList <- function(model, thetas, pointList,nemupts, nmodelpoints, np
             as.double(thetas),
             as.integer(nthetas),
             finaly = double(nemupts),
-            finalvar = double(nemupts)
+            finalvar = double(nemupts),
+            as.integer(cov.fn),
+            as.integer(reg.order)
             )
   results <- list(des=pointList, mean=res$finaly, var=res$finalvar)
 }
 
-## compute the lhood of a set of points in hyper-param space
-callEvalLhoodList <- function(model, pointList, nevalPoints, nmodelPoints, nparams=1, nthetas=3){
+## compute the lhood of a set of points in hyper-param space, given a set of thetas, a cov.fn and a regression
+## model
+## 
+## @param pointList an R matrix (nrow=nevalPoints, ncol=nthetas) of the locns in the hyperparam space to
+## estimate the log lhood
+## @param nevalPoints how many locations
+## @return a list res(des, lhood)
+## @return res$des <- pointList
+## @return res$lhood vector of lhoods at each location in pointList
+callEvalLhoodList <- function(model, pointList, nevalPoints, nmodelPoints, nparams=1, nthetas=3,
+                              cov.fn=1, reg.order=1){
+  checkCovFn(nthetas, nparams, cov.fn)
+  
   answer <- 0.0
   
   res <- .C("callEvalLhoodList",
@@ -108,15 +188,42 @@ callEvalLhoodList <- function(model, pointList, nevalPoints, nmodelPoints, npara
             as.double(model$training),
             as.integer(nmodelPoints),
             as.integer(nthetas),
-            finalLhood = double(nevalPoints)
+            finalLhood = double(nevalPoints),
+            as.integer(cov.fn),
+            as.integer(reg.order)
             )
 
   results <- list(des=pointList, lhood=res$finalLhood)
 }
 
+##
+## not to be externally called, checks that the cov.fn and nthetas given match up
+## should be called before doing any .C calls into rbind
+checkCovFn <- function(nthetas, nparams, cov.fn){
+  if(cov.fn < 1 || cov.fn > 3){
+    buffer <- paste("cov.fn index out of range: ", cov.fn, sep="")
+    stop(buffer)
+  }
+
+  if(cov.fn == 1){ ## power exp
+    if(nthetas != nparams + 2){
+      buffer <- paste("power exp with nparams: ", nparams, "needs nthetas: ", nparams+2, sep="")
+      stop(buffer)
+    }
+  } else { ## matern
+    if(nthetas != 3){
+      buffer <- paste("matern class need nthetas = 3, supplied: ", nthetas)
+      stop(buffer)
+    }
+  }
+}
+    
+
+  
+
 ## deprecated fns here
 callEmulate <- function(model, thetas, nmodelpts, nparams=1, nthetas=3, nemupts=20, rangemin=0.0, rangemax=1.0){
-  stop("deprecated, use callEmulateAtPoint or callEmulateAtList")
+  stop("callEmulate is deprecated, use callEmulateAtPoint or callEmulateAtList")
 }
 
 callcode <- function(model, nmodelpts, nparams=1, nthetas=3, nemupts=50, rangemin=0.0, rangemax=4.0){
@@ -127,21 +234,4 @@ callEvalLikelyhood <- function(model, nmodelpoints, vertex, nparams=1,nthetas=3)
   stop("callEvalLikelyhood is deprecated, use callEvalLhoodList")
 }
 
-## interpolate the data given by xvec and yvec at the point
-## xinterp, we need xvec and yvec to be the same length 
-##
-callInterpolate <- function(xvec, yvec, xinterp){
-  if(length(xvec) == length(yvec)){
-    yinterp <- .C("lagrange_interp",
-                as.double(xvec),
-                as.double(yvec),
-                as.integer(length(xvec)),
-                as.double(xinterp))
-                
-  } else {
-    print("error xvec not same length as yvec")
-    yinterp <- 0.0
-  }
-  yinterp[[4]][1]
-}
 
