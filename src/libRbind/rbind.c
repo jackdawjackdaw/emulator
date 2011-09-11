109ef1828dd32f640287762c89b2ee646e3145bc
ccs <cec24@phy.duke.edu>
2011-09-11 17:36:00 -0400

    removed deprecated functions

--- src/libRbind/rbind.c
+++ src/libRbind/rbind.c
@@ -1,6 +1,14 @@
 #include "rbind.h"
 
 /**
+ * \todo make similar or wrapper functions which would allow any C compatible ffi 
+ * without R's weirdness about vectors etc to setup and use the emulator
+ * 
+ * intended uses: python, paraview, scott? 
+ */
+
+
+/**
  * estimate the optimum covariance length scales to reproduce the training data
  * 
  * the user can force the nugget to be fixed, in this case its value will not be optimized
@@ -462,135 +470,3 @@ void testConvert(double* matrix, int *nx_in, int *ny_in){
 }
 
 
-/// graveyard ////
-
-
-/** 
- * this is deprecated 
- * use callEstimate to get the thetas you need and then callEmulateAtPt or callEmulateAtList to
- * get the predicted mean(s) and variance(s) you desire.
- */
-/* void callEmulator(double* xmodel_in, int *nparams_in, double* training_in, int *nmodelpts, int *nthetas, double* final_x, int* nemupts, \ */
-/* 									double* finaly, double* finalvar, double* rangemin, double* rangemax){ */
-/* 	double *thetas_vec; */
-/* 	int no_nugget  = 0; */
-/* 	thetas_vec = MallocChecked(sizeof(double)*(*nthetas)); */
-/* 	// this will run the estimator and fill in the thetas vector with the best ones */
-/* 	callEstimate(xmodel_in, nparams_in, training_in, nmodelpts, nthetas, thetas_vec, &no_nugget, NULL); */
-/* 	callEmulate(xmodel_in, nparams_in, training_in, nmodelpts, thetas_vec, nthetas, final_x, nemupts, finaly, finalvar, rangemin, rangemax); */
-/* 	free(thetas_vec); */
-/* } */
-
-
-
-/**
- * run the emulator against a given set of data and given hyperparams theta
- * this is deprecated, use callEmulateatPt or callEmulateatList
- */
-/* void callEmulate(double* xmodel_in, int* nparams_in, double* training_in, int* nmodelpts, double* thetas_in, int* nthetas_in, double* final_emulated_x, int *nemupts_in, double* final_emulated_y, double* final_emulated_variance, double* range_min_in, double*range_max_in){ */
-	
-/* 	optstruct options; */
-/* 	modelstruct the_model; */
-/* 	resultstruct results; */
-
-/* 	int i, j; */
-/* 	options.nmodel_points = *nmodelpts; */
-/* 	options.nparams = *nparams_in; */
-/* 	options.nemulate_points = *nemupts_in; */
-/* 	options.nthetas = *nthetas_in; */
-/* 	// fuck this needs to be set automatically :( */
-/* 	options.nregression_fns = 1 + options.nparams; */
-/* 	options.emulate_min = *range_min_in; */
-/* 	options.emulate_max = *range_max_in; */
-/* 	setup_cov_fn(&options); */
-
-
-/* 	alloc_modelstruct(&the_model, &options); */
-
-/* 	alloc_resultstruct(&results, &options); */
-	
-/* 	// fill in thetas */
-/* 	convertDoubleToVector(the_model.thetas, thetas_in, options.nthetas); */
-/* 	// fill in xmodel  */
-/* 	convertDoubleToMatrix(the_model.xmodel, xmodel_in, options.nparams, options.nmodel_points); */
-/* 	// fill in the training vec */
-/* 	convertDoubleToVector(the_model.training_vector, training_in, options.nmodel_points); */
-
-/* 	// first we have to fill the sample ranges */
-/* 	fill_sample_scales(&the_model, &options); */
-/* 	setup_optimization_ranges(&options, &the_model);	// this strictly isn't needed for emulator */
-
-
-/* 	// and call out to libEmu */
-/* 	emulate_model_results(&the_model, &options, &results); */
-	
-/* 	// Fill in emulated_y, emulated_variance */
-/* 	for(i = 0; i < options.nemulate_points; i++){ */
-/* 		final_emulated_y[i] = gsl_vector_get(results.emulated_mean, i); */
-/* 		final_emulated_variance[i] = gsl_vector_get(results.emulated_var, i); */
-/* 	} */
-
-/* 	// fill in final emulated_x */
-/* 	for(j = 0; j < options.nparams; j++){ */
-/* 		for(i = 0; i < options.nemulate_points; i++){ */
-/* 			final_emulated_x[i+j*options.nmodel_points] = gsl_matrix_get(results.new_x, i, j); */
-/* 		} */
-/* 	} */
-
-/* 	printf("hello from rbind-emulator\n"); */
-
-/* 	for(i = 0; i < options.nthetas; i++) */
-/* 		printf("%g\t", gsl_vector_get(the_model.thetas, i)); */
-/* 	printf("\n"); */
-					 
-/* 	printf("nemulate pts = %d\n", options.nemulate_points); */
-
-/* 	// check that the results struct agrees with final_emulated etc */
-/* 	for(i = 0; i < options.nemulate_points; i++){ */
-/* 		for(j = 0; j < options.nparams; j++){ */
-/* 			printf("%d:%g\t%g\t", i+j*options.nemulate_points, gsl_matrix_get(results.new_x, i, j), final_emulated_x[i+j*options.nemulate_points]); */
-/* 		} */
-		
-/* 		printf("%g\t%g\t", gsl_vector_get(results.emulated_mean, i), final_emulated_y[i]); */
-/* 		printf("%g\t%g\n", gsl_vector_get(results.emulated_var, i), final_emulated_variance[i]); */
-/* 	} */
-	
-
-/* 	// tidy up */
-/* 	free_modelstruct(&the_model); */
-/* 	free_optstruct(&options); */
-/* 	free_resultstruct(&results); */
-/* } */
-
-
-
-//! creates the coeffs for a lagrange poly interpolation
-/**
- *  @param xin are the points at which we require our polynomial to pass through
- *  @param valin are the values of the function we wish to interpolate at xin
- *  @param npts how many of xin there are, this also determines the order of the L poly
- *  @param where we want this to be evaluated
- *  @return desired_point_in is set to the correct value
- *
- * this is clearly possible in R but i just can't quite get it right
- */
-/* void lagrange_interp(double* xin, double* valin, int* npts_in, double* desired_point_in){ */
-/* 	int i, j; */
-/* 	double retVal = 0; */
-/* 	double weight = 0.0; */
-/* 	double npts = *npts_in; */
-/* 	double desired_point = *desired_point_in; */
-	
-/* 	for(i=0; i<npts; ++i){ */
-/* 		weight = 1.0; */
-/* 		for(j=0; j < npts; ++j){ */
-/* 			if( j != i){ */
-/* 				weight *= (desired_point - xin[j]) / (xin[i] - xin[j]); */
-/* 			} */
-/* 		} */
-/* 		retVal += weight*valin[i]; */
-/* 	} */
-/* 	// push the answer back into the desired_point_in */
-/* 	*desired_point_in = retVal; */
-	
-/* } */
