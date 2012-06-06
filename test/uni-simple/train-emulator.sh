94d98bacf56b47a4a283d02cd4f2a13153a386e0
ccs <cec24@phy.duke.edu>
2012-06-05 23:37:15 -0400

    added some testing

---
+++ test/uni-simple/train-emulator.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+## ccs, train an emulator on input_model_file.dat using a first order regression
+##
+## binpath reaches into build, is this a bad idea?
+binpath=../../build/src/interactive_emulator
+$binpath estimate_thetas input_model_file.dat univariate_snapshot_file --regression_order=1
