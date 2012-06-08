afc21a05c862e31d6288adfed3d6410d2aaad0d8
jackdawjackdaw <cec24@phy.duke.edu>
2012-06-08 11:47:34 -0400

    added training script for the 2d test

---
+++ test/hal-test/train-emulator.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+## ccs, train an emulator on input_model_file.dat using a first order regression
+##
+## binpath reaches into build, is this a bad idea?
+binpath=../../build/src/interactive_emulator
+inputfile=./Latin_square_sampling_2d_samp_fn_200.dat
+outputfile=M.dat
+## 
+$binpath estimate_thetas $inputfile $outputfile --regression_order=1
