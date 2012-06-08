2cb32a20ef8e3fc669b790353540e21297ca0cea
jackdawjackdaw <cec24@phy.duke.edu>
2012-06-08 11:08:35 -0400

    updated univariate test scripts, seems to work fine in 1d

---
+++ test/uni-simple/plot-results.R
@@ -0,0 +1,13 @@
+# read the output from sample-emulator.sh
+samples <- read.table("./sample_locations.dat")
+emu.mean <- read.table("emu_mean.dat")
+emu.var <- read.table("emu_var.dat")
+
+model <- read.table("input_model_file.dat", skip=2)
+
+plot(samples$V1, emu.mean$V1, type="l", col="blue", ylim=c(-0.1,1.5), xlab="x", ylab="y")
+points(t(model), pch=19)
+lines(samples$V1, emu.mean$V1+1.956*sqrt(emu.var$V1), col="red")
+lines(samples$V1, emu.mean$V1-1.956*sqrt(emu.var$V1), col="red")
+legend("topright", c("mean", "confidence", "training"), lty=c(1,1, -1), col=c("blue", "red", "black"), pch=c(-1,-1,19))
+title(main="simple 1d emulator test")
--- test/uni-simple/sample-emulator.sh
+++ test/uni-simple/sample-emulator.sh
@@ -1,6 +1,16 @@
-#!/bin/sh
+#!/bin/zsh
 ## ccs, generate samples from the trained model
 ##
 ## binpath reaches into build, is this a bad idea?
 binpath=../../build/src/interactive_emulator
-$binpath interactive_mode univariate_snapshot_file 
+$binpath interactive_mode univariate_snapshot_file < sample_locations.dat > temp.dat
+## now strip out the even/odd lines
+awk '(NR % 2 == 0 && NR > 5)' temp.dat > emu_mean.dat
+awk '(NR % 2  && NR > 5)' temp.dat > emu_var.dat
+
+## clear up
+if [ -f temp.dat ]; then
+		rm temp.dat
+fi
+
+## the R script plot-results.R will then make a nice plot if you want
