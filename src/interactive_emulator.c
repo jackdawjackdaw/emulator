5100442a7f319d01f0769bbfe71e3d82e8ef9cd0
Hal Canary <hal@cs.unc.edu>
2012-06-11 15:53:20 -0400

    fixed up multi-emu, but breaks during alloc_multimodelstruct whist calculating PCA.

--- src/interactive_emulator.c
+++ src/interactive_emulator.c
@@ -47,57 +47,41 @@ USE:
     --regression_order=1
     --regression_order=2
     --regression_order=3
-    --covariance_fn=POWER_EXPONENTIAL
+    --covariance_fqn=POWER_EXPONENTIAL
     --covariance_fn=MATERN32
     --covariance_fn=MATERN52
   The defaults are regression_order=0 and covariance_fn=POWER_EXPONENTIAL.
 
   These options will be saved in MODEL_SNAPSHOT_FILE.
 
-INPUT_MODEL_FILE FORMAT:
+INPUT_MODEL_FILE (With multi-output) FORMAT:
+  Models with multivariate output values y = y_1...y_t which we will think of as rows of a matrix
   BEGIN EXAMPLE
-    nparams
-    nmodel_points
+    number_outputs
+    number_params 
+    number_model_points
     X[0,0]
     ...
-    X[0,nparams-1]
+    X[0,number_params-1]
     X[1,0]
     ...
-    X[nmodel_points-1,nparams-1]
-    y[0]
+    X[number_model_points-1,number_params-1]
+    Y[0, 0]
+    Y[0, 1]
     ...
-    y[nmodel_points-1]
-  END EXAMPLE
+    Y[0, number_outputs-1]
+    ...
+    Y[1, 1]
+    ... 
+    Y[number_model_points-1, number_outputs-1]
+   END EXAMPLE
 
-  nparams and nmodel_points should be positive integers.  X[i,j] and
-  y[i] will be read as double-precision floats.
+  number_outputs, number_params and number_model_points should be
+  positive integers.  X[i,j] and Y[i,j] will be read as
+  double-precision floats.
 
   You don't have to use newlines to separate values.  If fscanf(fp,
   "%lf%*c", ptr) can read the value, it will work.
-
-INPUT_MODEL_FILE_MULTI FORMAT:
-  Models with multivariate output values y = y_1...y_t which we will think of as rows of a matrix
-  BEGIN EXAMPLE
-   nt
-   nparams 
-   nmodel_points
-    X[0,0]
-    ...
-    X[0,nparams-1]
-    X[1,0]
-    ...
-    X[nmodel_points-1,nparams-1]
-    y[0, 0]
-    y[0, 1]
-    ...
-    y[0, t-1]
-    ...
-    y[1, 1]
-    ... 
-    y[nmodel_points-1, t-1]
-   END EXAMPLE
-   
-   nt, nparams and nmodel_points should be positive ints, X_ij and Y_i,j are read as doubles
 
 BUGS:
   Writing MODEL_SNAPSHOT_FILE to stdout is not allowed because
@@ -164,59 +148,30 @@ static const char useage [] =
 	"The defaults are regression_order=0 and covariance_fn=POWER_EXPONENTIAL.\n";
 
 
-/********************************************************************/
+/**
+ * Convienence function for exiting a function.
+ */
 int perr(const char * s) {
 	fprintf(stderr,"%s\n",s);
 	return EXIT_FAILURE;
 }
 
 
-
-
-/*********************************************************************
-Reads a file containing xmodel and training_vector info and create
-data structures to hold that information.
-@param input_filename: name of file to open.
-  can be "-" or "stdin" to represent standard input
-@param xmodel_ptr: returns a newly allocated gsl_matrix
-@param training_vector_ptr: returns a newly allocated gsl_vector
-*********************************************************************/
+/**
+ * Reads a file containing xmodel and training_matrix info and create
+ * data structures to hold that information.
+ * @param input_filename: name of file to open.
+ *   can be "-" or "stdin" to represent standard input
+ * @param xmodel_ptr: returns a newly allocated gsl_matrix (nmodel_points x nparams)
+ * @param training_matrix_ptr: returns a newly allocated gsl_matrix (nmodel_points x nt)
+ * @returns 1 on sucess and 0 on error.
+ * 
+ * Example of use:
+ * 	gsl_matrix * xmodel;
+ * 	gsl_matrix * training_matrix;
+ * 	open_model_file(filename, &xmodel, &training_matrix)
+ */
 int open_model_file(char * input_filename,
-		gsl_matrix ** xmodel_ptr, gsl_vector ** training_vector_ptr) {
-	FILE * input_file;
-	if ((0 == strcmp(input_filename, "-")) || (0 == strcmp(input_filename, "stdin")))
-		input_file = stdin;
-	else
-		input_file = fopen(input_filename, "r");
-	if (input_file == NULL)
-		return 0; /* failure */
-	int i, j, nparams, nmodel_points;
-	fscanf(input_file,"%d%*c", & nparams);
-	fscanf(input_file,"%d%*c", & nmodel_points);
-	gsl_matrix * xmodel = gsl_matrix_alloc(nmodel_points, nparams);
-	gsl_vector * training_vector = gsl_vector_alloc(nmodel_points);
-	for (i = 0; i < nmodel_points; i++)
-		for (j = 0; j < nparams; j++)
-			fscanf(input_file, "%lf%*c", gsl_matrix_ptr(xmodel,i,j));
-	for (i = 0; i < nmodel_points; i++)
-		fscanf(input_file, "%lf%*c", gsl_vector_ptr(training_vector, i));
-	if (input_file != stdin)
-		fclose(input_file);
-	*xmodel_ptr = xmodel;
-	*training_vector_ptr = training_vector;
-	return 1;
-}
-
-
-/*********************************************************************
-Reads a file containing xmodel and training_matrix info and create
-data structures to hold that information.
-@param input_filename: name of file to open.
-  can be "-" or "stdin" to represent standard input
-@param xmodel_ptr: returns a newly allocated gsl_matrix (nmodel_points x nparams)
-@param training_matrix_ptr: returns a newly allocated gsl_matrix (nmodel_points x nt)
-*********************************************************************/
-int open_model_file_multivar(char * input_filename,
 		gsl_matrix ** xmodel_ptr, gsl_matrix ** training_matrix_ptr) {
 	FILE * input_file;
 	if ((0 == strcmp(input_filename, "-")) || (0 == strcmp(input_filename, "stdin")))
@@ -225,18 +180,18 @@ int open_model_file_multivar(char * input_filename,
 		input_file = fopen(input_filename, "r");
 	if (input_file == NULL)
 		return 0; /* failure */
-	int i, j, nt, nparams, nmodel_points;
-	fscanf(input_file,"%d%*c", & nt);
-	fscanf(input_file,"%d%*c", & nparams);
-	fscanf(input_file,"%d%*c", & nmodel_points);
-	gsl_matrix * xmodel = gsl_matrix_alloc(nmodel_points, nparams);
-	gsl_matrix * training_matrix = gsl_matrix_alloc(nmodel_points, nt);
+	int i, j, number_outputs, number_params, number_model_points;
+	fscanf(input_file,"%d%*c", & number_outputs);
+	fscanf(input_file,"%d%*c", & number_params);
+	fscanf(input_file,"%d%*c", & number_model_points);
+	gsl_matrix * xmodel = gsl_matrix_alloc(number_model_points, number_params);
+	gsl_matrix * training_matrix = gsl_matrix_alloc(number_model_points, number_outputs);
 	
-	for (i = 0; i < nmodel_points; i++)
-		for (j = 0; j < nparams; j++)
+	for (i = 0; i < number_model_points; i++)
+		for (j = 0; j < number_params; j++)
 			fscanf(input_file, "%lf%*c", gsl_matrix_ptr(xmodel,i,j));
-	for (i = 0; i < nmodel_points; i++)
-		for(j = 0; j < nt; j++)
+	for (i = 0; i < number_model_points; i++)
+		for(j = 0; j < number_outputs; j++)
 			fscanf(input_file, "%lf%*c", gsl_matrix_ptr(training_matrix, i, j));
 	
 	if (input_file != stdin)
@@ -249,17 +204,15 @@ int open_model_file_multivar(char * input_filename,
 
 
 
-
-
-/*********************************************************************
-Return true only if the beginning of s1 matches s2.
-*********************************************************************/
+/**
+ * Return true only if the beginning of s1 matches s2.
+ */
 #define starts_with(s1,s2) (strncmp ((s1), (s2), strlen(s2)) == 0)
 
 
-/*********************************************************************
-Return true only if s1 equals s2.
-*********************************************************************/
+/**
+ * Return true only if s1 equals s2.
+ */
 #define str_equal(s1,s2) (strcmp ((s1), (s2)) == 0)
 
 /**
@@ -304,84 +257,37 @@ void parse_arguments_interactive(int* cov_fn_index, int* regression_order, int a
 }
 
 
-/*********************************************************************
-If main called with "estimate_thetas" argument, this happens.
-*********************************************************************/
-int estimate_thetas (int argc, char ** argv) {
-	/* read a file into a GSL_matrix and a GSL_vector */
-	gsl_matrix * xmodel;
-	gsl_vector * training_vector;
-
-	if (argc < 2)
-		return perr("Not enough arguments\n");
-
-	if (! open_model_file(argv[0], &xmodel, &training_vector))
-		return perr("File read failed.");
-
-	/*
-	FILE * outfp;
-	if ((argc < 2) || (0 == strcmp(argv[1], "-")) || (0 == strcmp(argv[1], "stdout")))
-		outfp = stdout;
-	else
-		outfp = fopen(argv[1],"w"); */
-	FILE * outfp = fopen(argv[1], "w");
-	if (outfp == NULL)
-		return perr("Opening output file failed.");
-
-	int cov_fn_index = POWEREXPCOVFN; /* POWEREXPCOVFN: 1, MATERN32: 2, MATERN52: 3 */
-	int regression_order = 0; /* 0, 1, 2, or 3 */
-	
-	parse_arguments_interactive(&cov_fn_index, &regression_order, argc, argv);
-
-	// debug
-	fprintf(stderr, "# cov_fn_index %d\n# regression_order %d\n", cov_fn_index, regression_order);
-	
-	modelstruct * model = alloc_modelstruct_2(xmodel, training_vector,
-		cov_fn_index, regression_order);
-
-	/* actually do the estimation using libEmu */
-	estimate_thetas_threaded(model, model->options);
-	
-	
-	/* write to file */
-	dump_modelstruct_2(outfp, model);
-	fclose(outfp);
-
-	gsl_matrix_free(xmodel);
-	gsl_vector_free(training_vector);
-	free_modelstruct_2(model);
-	return 0;
-}
-
 /**
  * if main called with "estimate_thetas_multi"
  */
-int estimate_thetas_multi (int argc, char ** argv) {
-	gsl_matrix *xmodel;
-	gsl_matrix *training_matrix;
+int estimate_thetas(int argc, char ** argv) {
+	gsl_matrix *xmodel = NULL;
+	gsl_matrix *training_matrix = NULL;
 	double varfrac = 0.95; // this could be set by an arg
 
 	if (argc < 2)
 		return perr("Not enough arguments\n");
 
-	if (! open_model_file_multivar(argv[0], &xmodel, &training_matrix))
+	if (! open_model_file(argv[0], &xmodel, &training_matrix))
 		return perr("File read failed.");
 
-	
 	FILE * outfp = fopen(argv[1], "w");
 	if (outfp == NULL)
 		return perr("Opening output file failed.");
 
 	int cov_fn_index = POWEREXPCOVFN; /* POWEREXPCOVFN: 1, MATERN32: 2, MATERN52: 3 */
 	int regression_order = 0; /* 0, 1, 2, or 3 */
+
 	
 	parse_arguments_interactive(&cov_fn_index, &regression_order, argc, argv);
 
 	/* allocate the multi-model, do the pca decomp
 	 * this is a little chatty on stderr
 	 */
-	multi_modelstruct * model = alloc_multimodelstruct(xmodel, training_matrix,
-																										 cov_fn_index, regression_order, varfrac);
+
+	multi_modelstruct * model = alloc_multimodelstruct(
+		xmodel, training_matrix,
+		cov_fn_index, regression_order, varfrac);
 
 	/* actually do the estimation using libEmu and write to file! */
 	estimate_multi(model, outfp);
@@ -389,50 +295,58 @@ int estimate_thetas_multi (int argc, char ** argv) {
 	fclose(outfp);
 
 	gsl_matrix_free(xmodel);
-	gsl_vector_free(training_matrix);
+	gsl_matrix_free(training_matrix);
 	free_multimodelstruct(model);
 	return EXIT_SUCCESS;
 }
 
 
-
-
-/*********************************************************************
-If main called with "interactive_mode" argument, this happens.
-*********************************************************************/
+/**
+ * If main called with "interactive_mode" argument, this happens.
+ */
 int interactive_mode (int argc, char** argv) {
+	(void) argc; /* cleanup compiler warning for unused parameter */
+	(void) argv; /* cleanup compiler warning for unused parameter */
 	FILE * interactive_input = stdin;
 	FILE * interactive_output = stdout;
 	int i, r, expected_r;
-	double the_mean, the_variance;
 
 	FILE * fp = fopen(argv[0],"r");
 	if (fp == NULL)
 		return perr("Error opening file");
-	modelstruct* model = load_modelstruct_2(fp);
+	multi_modelstruct *model = load_multi_modelstruct(fp);
+
 	fclose(fp);
 
-	emulator_struct * the_emulator = alloc_emulator_struct(model);
+	multi_emulator *the_multi_emulator = alloc_multi_emulator(model);
 
-	int nparams = model->options->nparams;
-	gsl_vector * the_point = gsl_vector_alloc(nparams);
+	int number_params = model->nparams;
+	gsl_vector * the_point = gsl_vector_alloc(number_params);
 
-	#ifdef BINARY_INTERACTIVE_MODE
+	int number_outputs = model->nt;
+	gsl_vector *the_mean = gsl_vector_alloc(number_outputs);
+	gsl_vector *the_variance = gsl_vector_alloc(number_outputs);
+
+  #ifdef BINARY_INTERACTIVE_MODE
 		r = expected_r = sizeof(double);
 	#else
 		r = expected_r = 1;
 	#endif
-	fprintf(interactive_output,"%d\n",nparams);
-	for(i = 0; i < nparams; i++) {
+	fprintf(interactive_output,"%d\n",number_params);
+	for(i = 0; i < number_params; i++) {
 		/* FIXME we may want parameter identifiers in the future. */
 		fprintf(interactive_output,"%s%d\n","param_",i);
 	}
-	int nreturns = 2; /* More models would mean more returns. */
+	int nreturns = 2 * number_outputs ; /* FIXME - also return the joint implausibility. */
 	fprintf(interactive_output,"%d\n",nreturns);
-	fprintf(interactive_output,"%s\n%s\n","mean","variance");
+	for(i = 0; i < number_outputs; i++) {
+		/* FIXME we may want output identifiers in the future. */
+		fprintf(interactive_output,"%s_%d\n%s_%d\n","mean",i,"variance",i);
+	}
+
 	fflush(interactive_output);
 	while (! feof(interactive_input)) {
-		for(i =0; (i < nparams) && (r == expected_r); i++) {
+		for(i =0; (i < number_params) && (r == expected_r); i++) {
 			#ifdef BINARY_INTERACTIVE_MODE
 				r = fread(gsl_vector_ptr(the_point, i), sizeof(double), 1, interactive_input);
 			#else
@@ -441,28 +355,26 @@ int interactive_mode (int argc, char** argv) {
 		}
 		if (r < expected_r) /* probably eof, otherwise error */
 			break;
-		emulate_point(the_emulator, the_point, &the_mean, &the_variance);
-		#ifdef BINARY_INTERACTIVE_MODE
-			fwrite(&the_mean, sizeof(double), 1, interactive_output);
-			fwrite(&the_variance, sizeof(double), 1, interactive_output);
-		#else
-			fprintf(interactive_output, "%.17f\n%.17f\n", the_mean, the_variance);
-		#endif
+		emulate_point_multi(the_multi_emulator, the_point, the_mean, the_variance);
+		for(i = 0; i < number_outputs; i++) {
+			#ifdef BINARY_INTERACTIVE_MODE
+				fwrite(gsl_vector_ptr(the_mean,i), sizeof(double), 1, interactive_output);
+				fwrite(gsl_vector_ptr(the_variance,i), sizeof(double), 1, interactive_output);
+			#else
+				fprintf(interactive_output, "%.17f\n", gsl_vector_get(the_mean,i));
+				fprintf(interactive_output, "%.17f\n", gsl_vector_get(the_variance,i));
+			#endif
+		}
 		fflush(interactive_output);
 	}
-	free_emulator_struct(the_emulator);
-	gsl_matrix_free(model->xmodel);
-	model->xmodel = NULL;
-	gsl_vector_free(model->training_vector);
-	model->training_vector = NULL;
-	free_modelstruct_2(model);
+	free_multi_emulator(the_multi_emulator);
+	free_multimodelstruct(model);
 	gsl_vector_free(the_point);
+	gsl_vector_free(the_mean);
+	gsl_vector_free(the_variance);
 	return 0;
 }
 
-int interactive_mode_multi (int argc, char** argv) {
-	return EXIT_FAILURE;
-}
 
 /*********************************************************************
 main
@@ -470,18 +382,9 @@ main
 int main (int argc, char ** argv) {
 	if (argc < 3)
 		return perr(useage);
-	
-	if (0 == strcmp(argv[1], "estimate_thetas"))
+	if (str_equal(argv[1], "estimate_thetas"))
 		return estimate_thetas(argc - 2, argv + 2);
-	if (0 == strcmp(argv[1], "interactive_mode"))
+	if (str_equal(argv[1], "interactive_mode"))
 		return interactive_mode(argc - 2, argv + 2);
-	/**
-	 * ccs, added some multivar support, the command line passing is going to get a bit cranky soon 
-	 */
-	if (0 == strcmp(argv[1], "estimate_thetas_multi"))
-		return estimate_thetas_multi(argc - 2, argv + 2);
-	if (0 == strcmp(argv[1], "interactive_mode_multi"))
-		return interactive_mode_multi(argc - 2, argv + 2);
-	
 	return perr(useage);
 }
