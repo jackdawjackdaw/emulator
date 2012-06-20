64ffb8abc64c1628f1f79cb0a9a6e79687bdc6b7
ccs <cec24@phy.duke.edu>
2012-06-20 10:38:47 -0400

    removed cruft from useful

--- src/useful.c
+++ src/useful.c
@@ -6,43 +6,12 @@
 #include "useful.h"
 
 
-//! takes a print statement and either prints it out, or logs it 
-/**
- * \note should finish implementing this, or find a nice logging lib to do it
- */
-void message(char* the_message, int level){
-	// for now it just prints
-	char buffer[128];
-	sprintf(buffer, "LOG: %s\n", the_message);
-	fprintf(stderr, "%s\n", the_message);
-}
-
-
-
 //! checks for null
 void *MallocChecked(size_t size){
 	void *r = malloc(size);
-
 	if( r == NULL)
 		unix_error("memory wasn't allocated");
-	
 	return(r);
-}
-
-// unix error fn
-void unix_error(char *msg){
-	fprintf(stderr, "%s: %s\n", msg, strerror(errno));
-	exit(0);
-}
-
-
-void posix_error(int code, char* msg){
-	fprintf(stderr, "%s: %s\n", msg, strerror(code));
-}
-
-void error(char *msg){
-	fprintf(stderr, "%s\n", msg);
-	exit(0);
 }
 
 //! print a vector to stderr
@@ -53,68 +22,6 @@ int i;
 	}
 	fprintf(stderr,"\n");
 }
-
-
-// memory management
-
-/** allocates a 2d array of ints
- * @arg size_x the x dimension of the array
- * @arg size_y the y dimension of the array
- * @arg init_val an initial value to set all entries in the array to
- * @return a pointer to the allocated array, don't forget to free this properly later!
- */
-int* alloc_2d_int_array(int size_x, int size_y, int init_val){
-	int i;
-	int *the_array;
-	int the_total_size = size_x*size_y;
-	the_array = MallocChecked(sizeof(int)*the_total_size);
-	
-	for (i = 0; i < the_total_size; i++){
-		the_array[i] = init_val;
-	}
-	
-	return(the_array);
-}
-
-/** allocates a 2d array of floats
- * @arg size_x the x dimension of the array
- * @arg size_y the y dimension of the array
- * @arg init_val an initial value to set all entries in the array to
- * @return a pointer to the allocated array, don't forget to free this properly later!
- */
-double* alloc_2d_double_array( int size_x, int size_y, double init_val){
-	double *local_array;
-	int i;
-	int the_total_size = size_x*size_y;
-	local_array = MallocChecked(sizeof(double)*the_total_size);
-	
-	for(i = 0; i < the_total_size; i++){
-		local_array[i] = init_val;
-	}
-	return(local_array);
-}
-
-
-void print_int_array(int* the_array, int n_x, int n_y){
-	int i, j;
-	for(i = 0; i < n_x; i++){
-		for(j = 0; j < n_y; j++){
-			printf("%d ", the_array[i+j*n_x]);
-		}
-		printf("\n");
-	}
-}
-
-void print_double_array(double* the_array, int n_x, int n_y){
-	int i, j;
-	for(i = 0; i < n_x; i++){
-		for(j = 0; j < n_y; j++){
-			printf("%g ", the_array[i+j*n_x]);
-		}
-		printf("\n");
-	}
-}
-
 
 // RNG 
 // tries to read from /dev/random, or otherwise uses the system time
@@ -154,54 +61,5 @@ unsigned long int get_seed_noblock(void){
 		fclose(devrandom);
 	}
 	return(seed);
-}
-
-
-/* binary io routines */
-// input
-void in_int(FILE *fptr, int *iptr){
-	if (fread((void*) iptr, sizeof(int), 1, fptr) != 1) {		
-		error( "in_int: fread failed!"); 
-	}
-}
-
-
-void in_double(FILE *fptr, double *dptr){
-	if (fread((void*) dptr, sizeof(double), 1, fptr) != 1){
-		error("in_double: fread failed!\n");
-	}
-}
-
-
-void in_vector(FILE *fptr, double *vec){ // reads in a 3d vector
-	if (fread((void*) vec, sizeof(double), NDIM, fptr) != NDIM){
-		error( "in_vec: fread failed!\n"); 
-	}
-}
-
-void in_blob(FILE *fptr, int n_things, double *vec){ // reads in n_things doubles
-	if (fread((void*) vec, sizeof(double), n_things, fptr) != (unsigned)n_things){
-		error( "in_blob: fread_failed!\n");
-	}
-}
-
-
-// output
-void out_int(FILE* fptr, int ival){
-	if(fwrite((void*) &ival, sizeof(int), 1, fptr) != 1){
-		error( "out_int: fwrite failed!\n"); 
-	}
-}
-
-void out_double(FILE* fptr, double dval){
-	if(fwrite((void*) &dval, sizeof(double), 1, fptr) != 1){
-		error( "out_double: fwrite failed!\n"); 
-	}
-}
-	
-void out_vector(FILE* fptr, double* vec){
-	if(fwrite((void*) vec, sizeof(double), NDIM, fptr) != NDIM){
-		error( "out_vec: fwrite failed!\n"); 
-	}
 }
 
--- src/useful.h
+++ src/useful.h
@@ -7,19 +7,6 @@
 #include "gsl/gsl_rng.h"
 #include "gsl/gsl_vector.h"
 
-
-/**
- *  puts a message in the log (or in the stderr)
- */
-void message(char* the_message, int level);
-
-//*******************
-// memory management
-//*******************
-double* alloc_2d_double_array(int size_x, int size_y, double init_val);
-int* alloc_2d_int_array(int size_x, int size_y, int init_val);
-void print_double_array(double* the_array, int n_x, int n_y);
-void print_int_array(int* the_array, int n_x, int n_y);
 void print_vector_quiet(gsl_vector *x, int n);
 //******************
 // tries to get a seed from /dev/random or otherwise uses the system time
@@ -27,28 +14,6 @@ void print_vector_quiet(gsl_vector *x, int n);
 unsigned long int get_seed(void);
 unsigned long int get_seed_noblock(void);
 
-//***************
-// binary io
-// some low level stuff, snarfed from treeio.c
-//**************
-void in_int(FILE *fptr, int *);
-// read in a single double
-void in_double(FILE *fptr, double *);
-// read in a vector of doubles
-void in_vector(FILE *fptr, double *);
-// read in N doubles
-void in_blob(FILE *fptr, int n_things, double *vec);
-
-void out_int(FILE *fptr, int);
-void out_double(FILE *fptr, double);
-void out_vector(FILE *fptr, double*);
-
-// this will change the way that out vec and in vec work
-#define NDIM 3
-
-
 // errors
 void *MallocChecked(size_t size);
-void posix_error(int code, char* msg);
-void unix_error(char *msg);
-void error(char *msg);
+
