74c60ab5c360d13d7e2c7d0763f10c84ec33a703
Chris Coleman-Smith <cec24@phy.duke.edu>
2013-03-26 11:52:39 -0400

    now a by-eye test suggests that the covmat output is ok

--- test/multi-simple/sample-emu.sh
+++ test/multi-simple/sample-emu.sh
@@ -1,3 +1,5 @@
 #!/bin/zsh
 echo "0.03 0.04 0.01\n0.05 0.02 0.01" > sample_pts
-~/local/bin/interactive_emulator interactive_mode multi_snapshot_file < sample_pts
+~/local/bin/interactive_emulator interactive_mode multi_snapshot_file < sample_pts 
+## now repeat with cov-mat-estimation
+#~/local/bin/interactive_emulator interactive_mode multi_snapshot_file --covmat < sample_pts
